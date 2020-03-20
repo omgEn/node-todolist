1349e7f172712fb49d76f204c68c98e999594ced
zyy <1127263614@qq.com>
2020-03-20 22:52:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## 环境
+
+* 启动数据库服务`mongod --dbpath "D:\mongo\data"`
+* 项目启动：`npm start`
+* 使用 Robo3T 可视化工具操作数据库
+* 使用 Postman 测试接口
+
+
+## 接口文档
+
+* POST /user/regist 注册
+* POST /user/login  登录
+* GET /user/getAllUsers  获取所有用户
+
+* POST /upload/img  图片上传
+
+* POST /article/create 创建与编辑
---
+++ routes/users.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require('../db/userModel')
+
+/* 注册 */
+router.post('/regist', function(req, res) {
+  let { username, password, password2 } = req.body
+  if(!username || !password || !password2) {
+    res.json({err: 1, msg: '注册信息不能为空'})
+    return
+  }
+  if (password !== password2) {
+    res.json({err: 1, msg:'两次密码输入不一致'})
+    return
+  }
+  // 第1步：先判断用户名是否已被注册
+  userModel.find({username}).then(arr=>{
+    if(arr.length > 0) {
+      res.json({err: 1, msg: '该用户名已被注册'})
+    } else {
+      // 第2步：注册
+      userModel.insertMany([{username, password}]).then(()=>{
+        res.json({err: 0, msg: 'success'});
+      }).catch(error=>{
+        res.json({err: 1, msg: 'fail'})
+      })
+    }
+  })
+})
+
+/* 登录 */
+router.post('/login', function(req, res) {
+  let { username, password } = req.body
+  // 使用token相关的算法库，来生成一个token
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  userModel.find({username,password}).then(arr=>{
+    if (arr.length > 0) {
+      res.json({err:0, msg:'success', data: {token, username}})
+    } else {
+      res.json({err:1, msg: 'fail'})
+    }
+  })
+})
+
+
+/* 获取用户列表：测试跨域 */
+router.get('/getAllUsers', function(req, res) {
+
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  // res.header("X-Powered-By",' 3.2.1')
+  // res.header("Content-Type", "application/json;charset=utf-8");
+
+  userModel.find().then(arr=>{
+    res.json({err:0, msg:'success', data: arr})
+  }).catch(err=>{
+    res.json({err:1, msg:'fail'})
+  })
+})
+
+module.exports = router;
